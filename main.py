ae838d5b4ddd40adc85db0c9c1af0ca08de14940
Aleksandr Chalov <alchalov@gmail.com>
2022-04-18 10:32:57 +0800

    Импорт библиотек

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import pycountry
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from yandex_translate import YandexTranslate # Используем класс YandexTranslate из модуля yandex_translate
+from yandex_translate import YandexTranslateException # Используем класс YandexTranslateException из модуля yandex_translate
